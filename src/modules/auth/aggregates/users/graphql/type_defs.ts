d5fef80a85f3e1106799ff8102075939b0a9d8d3
Neoflies <tienloinguyen22@gmail.com>
2020-01-12 15:48:17 +0700

    Fix type in graphql definition

--- src/modules/auth/aggregates/users/graphql/type_defs.ts
+++ src/modules/auth/aggregates/users/graphql/type_defs.ts
@@ -2,7 +2,7 @@ import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
   extend type Query {
-    users: QueryUsersOperations;
+    users: QueryUsersOperations
   }
 
   type QueryUsersOperations {
